2a3698f1ebf17934927e7a705266dce23818d420
baekdoosan <doosan0425@gmail.com>
2024-12-17 13:04:51 +0900

    ### 비관적 락을 사용하여 한 번에 하나의 트랜잭션만 특정 행을 업데이트하도록 제한
    ### 내용
    ProductRepository.java :
    비관적 락을 사용하여 한 번에 하나의 트랜잭션만 특정 행을 업데이트하도록 제한

--- src/main/java/com/doosan/notification/repository/ProductRepository.java
+++ src/main/java/com/doosan/notification/repository/ProductRepository.java
@@ -1,9 +1,23 @@
 package com.doosan.notification.repository;
 
 import com.doosan.notification.entity.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+// 비관적 락을 사용하여 한 번에 하나의 트랜잭션만 특정 행을 업데이트하도록 제한
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 비관적 락(Pessimistic Lock) 사용
+    @Lock(LockModeType.PESSIMISTIC_WRITE) // @Lock 어노테이션 추가
+    @Query("SELECT p FROM Product p WHERE p.id = :productId")
+    Optional<Product> findByIdWithLock(Long productId);
+
+
+
+
 }
